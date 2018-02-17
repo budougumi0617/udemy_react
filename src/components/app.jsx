01e116410ad13555976722008852cfa265574c10
budougumi0617 <budougumi0617@gmail.com>
2018-02-17 09:00:00 +0900

    :+1: Done 2-18

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,11 +3,30 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'John',
+    };
+  }
+
+  handleMouseOver() {
+    this.setState({ name: 'Bob' });
+  }
+
+  handleMouseOut() {
+    this.setState({ name: 'Mike' });
+  }
+
   render() {
     return (
-      <div>
-        <Greeting name="Bob" />
-        <Greeting name="John" />
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onFocus={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+        onBlur={() => this.handleMouseOut()}
+      >
+        <Greeting name={this.state.name} />
       </div>
     );
   }
