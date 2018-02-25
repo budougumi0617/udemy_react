488691199e3fb9e96c794c2f4551facf1257b73a
budougumi0617 <budougumi0617@gmail.com>
2018-02-25 20:04:11 +0900

    :+1: Done 2-23

--- src/components/GeocodeResult.jsx
+++ src/components/GeocodeResult.jsx
@@ -1,8 +1,9 @@
 import React, { PropTypes } from 'react';
 
-
+// 引数をpropsではなく、受け取りたいpropsの変数名で受ければ、
+// props.addressと言った呼び出しをする必要がなくなる
 const GeocodeResult = ({ address, lat, lng }) => (
-  <ul>
+  <ul className="geocode-result">
     <li>住所:{address}</li>
     <li>緯度:{lat}</li>
     <li>経度:{lng}</li>
@@ -15,6 +16,7 @@ GeocodeResult.propTypes = {
   lng: PropTypes.number,
 };
 
+// requiredじゃないときはデフォルト引数の設定が必要。
 GeocodeResult.defaultProps = {
   address: '',
   lat: 0,
