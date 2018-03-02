df1122b79a258066f15df4fcd006ac3962eeb55c
budougumi0617 <budougumi0617@gmail.com>
2018-03-02 22:14:00 +0900

    W.I.P. 3-25

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 const InnerMap = withGoogleMap(props => (
   <GoogleMap
-    ref={props.onMapLoad}
-    defaultZoom={3}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={12}
+    defaultCenter={props.position}
+    center={props.position}
   >
   </GoogleMap>
 ));
 
-const Map = props => (
+const Map = ({ lat, lng }) => (
   <InnerMap
     containerElement={(<div />)}
     mapElement={(<div className="map"/>)}
+    position={{ lat, lng }}
   />
 );
+
+Map.propTypes = {
+  lat: PropTypes.number,
+  lng: PropTypes.number,
+}
+
+Map.defaultProps = {
+  lat: 35.6585805,
+  lng: 139.7454329,
+}
 
 export default Map;
