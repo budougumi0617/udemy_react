b2aec1eb3f013ad221a4787714731ff2d5b61e17
budougumi0617 <budougumi0617@gmail.com>
2018-03-03 10:44:37 +0900

    :+1: Set marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,16 +7,21 @@ const InnerMap = withGoogleMap(props => (
     defaultCenter={props.position}
     center={props.position}
   >
+    <Marker {...props.marker} />
   </GoogleMap>
 ));
 
-const Map = ({ lat, lng }) => (
-  <InnerMap
-    containerElement={(<div />)}
-    mapElement={(<div className="map" />)}
-    position={{ lat, lng }}
-  />
-);
+const Map = ({ lat, lng }) => {
+  const position = { lat, lng };
+  return (
+    <InnerMap
+      containerElement={(<div />)}
+      mapElement={(<div className="map" />)}
+      position={position}
+      marker={{ position }}
+    />
+  );
+};
 
 Map.propTypes = {
   lat: PropTypes.number,
