26f8e83411e2eca0c285cfb2a94c9ef1dac35eba
budougumi0617 <budougumi0617@gmail.com>
2018-03-08 09:38:44 +0900

    :tada: Done 3-30 Add review, price, and distance

--- src/components/HotelRow.jsx
+++ src/components/HotelRow.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// 極力コンポーネントはHTMLにマッピングする程度に済ませること。
 const HotelRow = ({ hotel }) => (
   <tr>
     <td><img src={hotel.thumbUrl} alt={hotel.name} /></td>
     <td><a href={hotel.url} target="_blank">{hotel.name}</a></td>
+    <td>{hotel.price}</td>
+    <td>{hotel.reviewAverage}</td>
+    <td>{hotel.reviewCount}</td>
+    <td>{hotel.distance}</td>
   </tr>
 );
 
@@ -14,6 +19,10 @@ HotelRow.propTypes = {
     name: PropTypes.string,
     url: PropTypes.string,
     thumbUrl: PropTypes.string,
+    price: PropTypes.string,
+    reviewAverage: PropTypes.number,
+    reviewCount: PropTypes.number,
+    distance: PropTypes.number,
   }).isRequired,
 };
 
--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,3 +1,4 @@
+import geolib from 'geolib';
 import Rakuten from '../lib/Rakuten';
 
 // Set AppID from https://webservice.rakuten.co.jp/app/list
@@ -14,13 +15,21 @@ export const searchHotelByLocation = (location) => {
   return Rakuten.Travel.simpleHotelSearch(params)
     .then(result =>
       result.data.hotels.map((hotel) => {
-        console.log(hotel);
         const basicInfo = hotel.hotel[0].hotelBasicInfo;
+        const price = basicInfo.hotelMinCharge;
+        const distance = geolib.getDistance(
+          { latitude: location.lat, longitude: location.lng },
+          { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
+        );
         return {
           id: basicInfo.hotelNo,
           name: basicInfo.hotelName,
           url: basicInfo.hotelInformationUrl,
           thumbUrl: basicInfo.hotelThumbnailUrl,
+          price: price ? `${price}円` : '空室なし',
+          reviewAverage: basicInfo.reviewAverage,
+          reviewCount: basicInfo.reviewCount,
+          distance, // short handが使える
         };
       }));
 };
