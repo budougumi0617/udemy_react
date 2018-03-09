e8514e888663dca9973ff8e9c6385d57c2d9f4e0
budougumi0617 <budougumi0617@gmail.com>
2018-03-09 09:26:13 +0900

    :+1: Add sort label by price

--- src/components/HotelsTable.jsx
+++ src/components/HotelsTable.jsx
@@ -3,14 +3,21 @@ import PropTypes from 'prop-types';
 
 import HotelRow from './HotelRow';
 
-const HotelsTable = ({ hotels, onSort }) => (
+const HotelsTable = ({ hotels, onSort, sortKey }) => (
   <table>
     <tbody>
       <tr>
         <th>画像</th>
         <th>ホテル名</th>
-        <th className="hotel-price-column">値段</th>
-        <th onClick={() => onSort('reviewAverage')}>レビュー</th>
+        <th
+          className="hotel-price-column"
+          onClick={() => onSort('price')}
+        >
+          値段{sortKey === 'price' ? '▲' : ''}
+        </th>
+        <th onClick={() => onSort('reviewAverage')}>
+          レビュー{sortKey === 'reviewAverage' ? '▲' : ''}
+        </th>
         <th>レビュー件数</th>
         <th>距離</th>
       </tr>
@@ -22,6 +29,7 @@ const HotelsTable = ({ hotels, onSort }) => (
 HotelsTable.propTypes = {
   hotels: PropTypes.arrayOf(PropTypes.any),
   onSort: PropTypes.func.isRequired,
+  sortKey: PropTypes.string.isRequired,
 };
 
 // requiredじゃないときはデフォルト引数の設定が必要。
